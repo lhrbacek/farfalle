2745f339f30f0b75fdc3fffe6fc4f163f37ce0f8
Lubomir Hrbacek <lubomirhrbacek@seznam.cz>
2022-06-04 13:14:24 +0200

    feat: Remove fetcher stored in bad directory

--- frontend/src/stories/models/fetcher.ts
+++
@@ -1,8 +0,0 @@
-function fetcher(url: string, header: Headers) {
-    return fetch(`http://localhost:4000/${url}`, {
-      headers: header,
-    }).then((response) => response.json());
-  }
-  
-  export default fetcher;
-  
