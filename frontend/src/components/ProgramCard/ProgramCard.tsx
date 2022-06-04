33134d654fd88bcefade2736cf8909941a364ad9
Lubomir Hrbacek <lubomirhrbacek@seznam.cz>
2022-06-04 15:56:27 +0200

    feat: FE: Add fetching of program

--- frontend/src/components/ProgramCard/ProgramCard.tsx
+++ frontend/src/components/ProgramCard/ProgramCard.tsx
@@ -3,18 +3,28 @@ import { Performance } from '../../types/performance';
 import { Container, Stack } from '@mantine/core';
 import LongPerformanceItem from './LongPerformanceItem';
 import { performances as data } from '../../data/performances'
+import useSWR from 'swr';
+import fetcher from '../../models/fetcher';
 
 export function ProgramCard() {
-  const values = data;  //TODO: Fetch from backend five newest performances
+  // const values = data;  //TODO: Fetch from backend five newest performances
 
   useEffect(() => {
     document.title = "Farfalle | Program"
   }, [])
 
+  const { data, error } = useSWR(`performance?future=true`, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  // TODO spinning wheel
+  if (!data) return <div>loading...</div>;
+
+  const performances: Performance[] = data
+
   return (
     <Container>
       <Stack spacing="xs">
-        {values.map((value: Performance) => <LongPerformanceItem key={value.id} {...value} />)}
+        {performances.map((performance: Performance) => <LongPerformanceItem key={performance.id} {...performance} />)}
       </Stack>
     </Container>
   );
