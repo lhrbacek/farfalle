ea837facf7e5862cec56d2baeb38c74badbcb098
adbi13 <adela.bierska@gmail.com>
2022-06-05 22:07:00 +0200

    Change API URL to environment variable

--- frontend/src/models/fetcher.ts
+++ frontend/src/models/fetcher.ts
@@ -1,8 +1,10 @@
-  function fetcher(url: string, header: Headers) {
-    return fetch(`http://localhost:4000/${url}`, {
-      headers: header,
-    }).then((response) => response.json());
-  }
-  
-  export default fetcher;
+const API_URL = process.env.API_URL ?? "http://api:3001/"
+
+function fetcher(url: string, header: Headers) {
+  return fetch(`${API_URL}${url}`, {
+    headers: header,
+  }).then((response) => response.json());
+}
+
+export default fetcher;
   
