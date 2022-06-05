c1d5f224cbe75f990133807ed4c682d3e4b2822c
Lubomir Hrbacek <lubomirhrbacek@seznam.cz>
2022-06-05 16:27:10 +0200

    feat: FE: Add fetching of Homepage

--- frontend/src/components/HomeCard/HomeCard.tsx
+++ frontend/src/components/HomeCard/HomeCard.tsx
@@ -1,23 +1,30 @@
 import { Container, Divider, Pagination, Stack, Title } from '@mantine/core';
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 import { plays as data } from '../../data/plays'
-import { Play } from '../../types/play';
+import fetcher from '../../models/fetcher';
+import { Play, PlayOverview } from '../../types/play';
 import PlayItem from './PlayItem';
 
 export function HomeCard() {
-  const values = data;  //TODO: Fetch from backend five newest performances
   const [activePage, setPage] = useState(1); // TODO: Add paging
 
   useEffect(() => {
     document.title = "Farfalle | Home"
   }, [])
 
+  const { data, error } = useSWR(`play?future=true`);
+  if (error) return <div>failed to load</div>;
+  // TODO spinning wheel
+  if (!data) return <div>loading...</div>;
+  const performances: PlayOverview[] = data
+
   return (
     <Container>
       <Title order={1} >Welcome to Farfalle!</Title>
       <Divider my="sm" />
       <Stack spacing="xs">
-        {values.map((value: Play) => <PlayItem key={value.id} {...value} />)}
+        {performances.map((play: Play) => <PlayItem key={play.id} {...play} />)}
         <Pagination page={activePage} onChange={setPage} total={2} color="dark" />
       </Stack>
     </Container>
