f431515335dc38e9ff7e87ff139eb625db65c228
Veronika Hanulíková <xhanulik@fi.muni.cz>
2022-06-05 20:31:11 +0200

    feat(cart): Filter atom according to reservationAt time

--- frontend/src/components/OrderPlacement/OrderTicketItem.tsx
+++ frontend/src/components/OrderPlacement/OrderTicketItem.tsx
@@ -47,6 +47,7 @@ export function OrderTicketItem({ ticket, removable }: OrderTicketItemProps) {
           <Text weight={700} component={Link} to={`/program/${ticket.performance.play.id}`}>{ticket.performance.play.name}</Text>
         </Group>
         <Group>
+          <Text>{ticket.id}</Text>
           <Text >{format(new Date(ticket.performance.dateTime), "dd.MM.yyyy, HH:mm")}</Text>
           <Text color="gray">Row: {ticket.row}</Text>
           <Text color="gray">Seat: {ticket.seat}</Text>
