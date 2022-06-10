25709260570e663e9e0795a85c19d38e6c210abc
Lubomir Hrbacek <lubomirhrbacek@seznam.cz>
2022-06-10 16:55:20 +0200

    fix: FE: Add credential flag to fetcher to allow cookies

--- frontend/src/models/fetcher.ts
+++ frontend/src/models/fetcher.ts
@@ -2,6 +2,7 @@ const API_URL = "http://0.0.0.0:4000/"
 
 function fetcher(url: string, header: Headers) {
   return fetch(`${API_URL}${url}`, {
+    credentials: "include",
     headers: header,
   }).then((response) => response.json());
 }
