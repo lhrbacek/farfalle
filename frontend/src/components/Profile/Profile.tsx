7c74c1b57cb9f4ec9b710f7e8da47bc722c301f7
Lubomir Hrbacek <lubomirhrbacek@seznam.cz>
2022-06-11 19:16:36 +0200

    feat: Get userId from lcoalStorage and show profile info (not done)

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -8,25 +8,34 @@ import LoadError from '../Error/LoadError';
 import LoadingCard from '../Loading/LoadingCard';
 import useSWR from 'swr';
 
+// TODO: Make better user type
+
 function Profile() {
   useEffect(() => {
     document.title = "Farfalle | Account"
   }, [])
 
-  // FETCH, send GET Request on API_URL/user/5 (base url can be found in src/models/fetcher)
-  const { data, error } = useSWR(`user/5`);
+  let userId = localStorage.getItem("userId");
+  if (userId === null) return <LoadError /> // no user logged
+
+  const { data, error } = useSWR(`user/${userId}`);
   if (error) return <LoadError />;
   if (!data) return <LoadingCard />;
-  console.log(data); // print acquired data, thats all
+  console.log(data); // print acquired data
+
+  let isAdmin = false;
+  if (data?.role == "ADMIN") {
+    isAdmin = true;
+  }
 
   return (
     <Container>
       <Group position={'center'} align={'start'}>
         <Paper withBorder shadow="md" p={30} radius="md">
           <Stack>
-            <Title>{user.name} {user.surname}</Title>
-            <Text size="md">Email: {user.email}</Text>
-            <Text size="md">Address: {user.address}</Text>
+            <Title>{data?.name} {data?.surname}</Title>
+            <Text size="md">Email: {data?.email}</Text>
+            <Text size="md">Address: TODO</Text>
             <Divider />
             <div hidden={!user.admin}>
               <Button color="gray" fullWidth rightIcon={<Database size={14} />} component={Link} to='admin'>
@@ -43,7 +52,8 @@ function Profile() {
         </Paper>
         <Stack justify="space-between">
           <Stack justify="space-between">
-            {user.orders.map((ticket) => <PurchasedTicket key={ticket.id} {...ticket} />)}
+            {/* {user.orders.map((ticket) => <PurchasedTicket key={ticket.id} {...ticket} />)} */}
+            TODO
           </Stack>
           <Card.Section>
             <Button color="dark" rightIcon={<Ticket size={14} />} component={Link} to='tickets'>
