1cd43353e06b9a8f9b5497c0434f67f77e082222
adbi13 <adela.bierska@gmail.com>
2022-06-13 10:59:18 +0200

    Add validation to ticket

--- backend/src/ticket/dto/create-ticket.dto.ts
+++ backend/src/ticket/dto/create-ticket.dto.ts
@@ -1,6 +1,18 @@
+import { IsPositive, IsInt } from 'class-validator';
+
 export class CreateTicketDto {
+  @IsPositive()
   price: number;
+
+  @IsInt()
+  @IsPositive()
   row: number;
+
+  @IsInt()
+  @IsPositive()
   seat: number;
+
+  @IsInt()
+  @IsPositive()
   performance: number;
 }
