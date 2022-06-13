b39db9598e5dac27fd5b8bd2e14900700fbd9a26
adbi13 <adela.bierska@gmail.com>
2022-06-13 11:04:51 +0200

    Add validation to user

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,7 +1,28 @@
+import {
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsPositive,
+  MaxLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
+  @MaxLength(254)
   email: string;
+
+  @IsNotEmpty()
+  @MaxLength(50)
   name: string;
+
+  @IsNotEmpty()
+  @MaxLength(50)
   surname: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsInt()
+  @IsPositive()
   address: number;
 }
--- backend/src/user/dto/update-user.dto.ts
+++ backend/src/user/dto/update-user.dto.ts
@@ -1,6 +1,28 @@
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+  MaxLength,
+} from 'class-validator';
+
 export class UpdateUserDto {
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(50)
   name?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(50)
   surname?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
   password?: string;
+
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
   address?: number;
 }
