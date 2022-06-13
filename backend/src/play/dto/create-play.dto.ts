a4cbf0224d029f7dee5eb553bba06ad16c3e2da4
adbi13 <adela.bierska@gmail.com>
2022-06-13 10:49:53 +0200

    Add validation do play

--- backend/src/play/dto/create-play.dto.ts
+++ backend/src/play/dto/create-play.dto.ts
@@ -1,7 +1,19 @@
+import { IsInt, IsPositive, IsUrl, MaxLength } from 'class-validator';
+
 export class CreatePlayDto {
+  @MaxLength(100)
   name: string;
+
+  @MaxLength(1000)
   description: string;
+
+  @IsUrl()
   imageURL: string;
+
+  @IsInt()
+  @IsPositive()
   lengthMinutes: number;
+
+  @MaxLength(100)
   director: string;
 }
