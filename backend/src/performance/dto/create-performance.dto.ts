ce86a53fedce49156629804ee3bfdf824a133ed7
adbi13 <adela.bierska@gmail.com>
2022-06-13 10:43:24 +0200

    Add validation to performance

--- backend/src/performance/dto/create-performance.dto.ts
+++ backend/src/performance/dto/create-performance.dto.ts
@@ -1,5 +1,14 @@
+import { IsDateString, IsInt, IsPositive } from 'class-validator';
+
 export class CreatePerformanceDto {
+  @IsDateString()
   dateTime: string;
+
+  @IsInt()
+  @IsPositive()
   play: number;
+
+  @IsInt()
+  @IsPositive()
   venue: number;
 }
--- backend/src/performance/dto/update-performance.dto.ts
+++ backend/src/performance/dto/update-performance.dto.ts
@@ -1,5 +1,17 @@
+import { IsDateString, IsInt, IsOptional, IsPositive } from 'class-validator';
+
 export class UpdatePerformanceDto {
+  @IsOptional()
+  @IsDateString()
   dateTime?: string;
+
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
   play?: number;
+
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
   venue?: number;
 }
