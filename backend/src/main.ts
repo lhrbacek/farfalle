8092e1262e766e129e6cc9603810da6755c4b091
adbi13 <adela.bierska@gmail.com>
2022-06-13 10:25:33 +0200

    Add Validation pipeline

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   //require("dotenv").config();
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: 'http://localhost',
     credentials: true,
