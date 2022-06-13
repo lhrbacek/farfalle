9dffcf3936b72fd2d86becdc20c8436d26181473
Veronika Hanulíková <xhanulik@fi.muni.cz>
2022-06-14 00:21:18 +0200

    feat: Add basic filtering and searching

--- frontend/src/components/ProgramCard/ProgramCard.tsx
+++ frontend/src/components/ProgramCard/ProgramCard.tsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { Performance } from '../../types/performance';
-import { Center, Container, Pagination, Stack } from '@mantine/core';
-import LongPerformanceItem from './LongPerformanceItem';
+import { ActionIcon, Container, Group, SegmentedControl, Stack, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import useSWR from 'swr';
 import LoadingCard from '../Loading/LoadingCard';
 import LoadError from '../Error/LoadError';
+import Performances from './Performances';
+import { ArrowRight, Search } from 'tabler-icons-react';
 
 export function ProgramCard() {
-  const [page, setPage] = useState(1);
-  const performancePerPage = 5;
+  const [filter, setFilter] = useState("all");
+  const [searchString, setSearchString] = useState("");
+
+  const form = useForm({
+    initialValues: { sString: "" },
+    validate: {},
+  });
 
   useEffect(() => {
     document.title = "Farfalle | Program"
@@ -19,22 +26,60 @@ export function ProgramCard() {
   if (error) return <LoadError />;
   if (!data) return <LoadingCard />;
 
-  const performances: Performance[] = data
+  const performances: Performance[] = data;
 
-  //pagination
-  const pages = Math.ceil(performances.length / performancePerPage);
-  const indexOfLastPlay = page * performancePerPage;
-  const indexOfFirstPlay = indexOfLastPlay - performancePerPage < 0 ? 0 : indexOfLastPlay - performancePerPage;
-  const currentPerformances = performances.slice(indexOfFirstPlay, indexOfLastPlay);
+  const searchPerformances = (searchString: string, performances: Performance[]) => {
+    if (searchString == "") {
+      return performances;
+    }
+    return performances.filter((item) => item.play.name.toLowerCase().includes(searchString));
+  }
 
+  const getPerformances = (searchString: string, filter: string, performances: Performance[]) => {
+    const searchedPerformances = searchPerformances(searchString, performances);
+    switch (filter) {
+      case "today":
+        return [searchedPerformances[0]];
+      default:
+        return searchedPerformances;
+    }
+  }
+
+  const handleSubmit = (values: { sString: string }) => {
+    setSearchString(values.sString.toLowerCase());
+  }
 
   return (
     <Container>
-      <Stack spacing="xs">
-        {currentPerformances.map((performance: Performance) => <LongPerformanceItem key={performance.id} {...performance} />)}
-        <Center>
-          <Pagination page={page} onChange={setPage} total={pages} siblings={0} color="dark" />
-        </Center>
+      <Stack>
+        <Group position='apart'>
+          <SegmentedControl
+            data={[
+              { value: 'all', label: 'All' },
+              { value: 'today', label: 'Today' },
+              { value: 'week', label: 'This week' },
+              { value: 'month', label: 'This month' },
+            ]}
+            onChange={(value) => setFilter(value)}
+          />
+          <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+            <TextInput
+              icon={<Search size={18} />}
+              color='red'
+              radius="xl"
+              rightSection={
+                <ActionIcon size={32} radius="xl" variant="filled" type="submit">
+                  <ArrowRight size={18} />
+                </ActionIcon>
+              }
+              {...form.getInputProps('sString')}
+              placeholder="Search performances"
+              rightSectionWidth={42}
+            />
+          </form>
+        </Group>
+
+        <Performances performances={getPerformances(searchString, filter, performances)} />
       </Stack>
     </Container>
   );
--- frontend/src/state/CartStateSelector.ts
+++ frontend/src/state/CartStateSelector.ts
@@ -1,12 +1,10 @@
 import { selector } from "recoil";
 import { cartState } from "./CartState";
-import { reservationTime } from "./reservationTime";
 
 export const cartStateSelector = selector({
     key: "cartStateSelector",
     get: ({ get }) => {
       const cart = get(cartState);
-      //const current = cart.filter((item) => (item.reservedAt == undefined ? false : (new Date().getTime() - (new Date(item.reservedAt)).getTime()) < reservationTime));
       return cart;
     },
 });
