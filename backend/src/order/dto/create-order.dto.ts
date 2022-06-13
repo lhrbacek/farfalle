74acde0767815c4e9c5a88e94c39d9f40410abef
adbi13 <adela.bierska@gmail.com>
2022-06-13 10:39:22 +0200

    Add validation to order

--- backend/src/order/dto/create-order.dto.ts
+++ backend/src/order/dto/create-order.dto.ts
@@ -1,6 +1,26 @@
+import {
+  ArrayUnique,
+  IsEmail,
+  IsInt,
+  IsOptional,
+  IsPositive,
+  MaxLength,
+} from 'class-validator';
+
 export class CreateOrderDto {
+  @IsEmail()
+  @MaxLength(254)
   email: string;
+
+  @ArrayUnique()
   tickets: number[];
-  adress: number;
+
+  @IsInt()
+  @IsPositive()
+  address: number;
+
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
   user?: number;
 }
