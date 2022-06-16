292cc07d8bd90f230e19a1d2541ae050fa81c873
Veronika Hanulíková <xhanulik@fi.muni.cz>
2022-06-16 23:59:30 +0200

    feat: add swagger for venue

--- backend/src/venue/dto/create-venue.dto.ts
+++ backend/src/venue/dto/create-venue.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty, MaxLength, Min } from 'class-validator';
 
 export class CreateVenueDto {
   @IsNotEmpty()
   @MaxLength(100)
+  @ApiProperty()
   name: string;
 
   @IsInt()
   @Min(1)
+  @ApiProperty()
   rows: number;
 
   @IsInt()
   @Min(1)
+  @ApiProperty()
   cols: number;
 }
--- backend/src/venue/venue.controller.ts
+++ backend/src/venue/venue.controller.ts
@@ -15,14 +15,29 @@ import { VenueService } from './venue.service';
 import { Venue as VenueModel } from '@prisma/client';
 import { AuthService } from 'src/auth/auth.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('venue')
+@ApiTags('venue')
 export class VenueController {
   constructor(
     private readonly venueService: VenueService,
     private readonly authService: AuthService,
   ) {}
 
+  @ApiOperation({ summary: 'Create one venue' })
+  @ApiResponse({
+    status: 200,
+    description: 'The created venue',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Not privileged',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal error',
+  })
   @UseGuards(JwtAuthGuard)
   @Post()
   async create(
@@ -33,16 +48,59 @@ export class VenueController {
     return await this.venueService.create(createVenueDto);
   }
 
+  @ApiOperation({ summary: 'Get all venues' })
+  @ApiResponse({
+    status: 200,
+    description: 'The found venues',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal error',
+  })
   @Get()
   async findAll() {
     return await this.venueService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get one venue' })
+  @ApiResponse({
+    status: 200,
+    description: 'The found venue',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal error',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'Should be an id of an venue',
+    type: Number,
+  })
   @Get(':id')
   async findOne(@Param('id') id: number) {
     return await this.venueService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Update one venue' })
+  @ApiResponse({
+    status: 200,
+    description: 'The updated venue',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Not privileged',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal error',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'Should be an id of an venue',
+    type: Number,
+  })
   @UseGuards(JwtAuthGuard)
   @Patch(':id')
   async update(
@@ -54,6 +112,21 @@ export class VenueController {
     return await this.venueService.update(id, updateVenueDto);
   }
 
+  @ApiOperation({ summary: 'Delete one venue' })
+  @ApiResponse({
+    status: 403,
+    description: 'Not privileged',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal error',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'Should be an id of an venue',
+    type: Number,
+  })
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: number, @Request() req) {
