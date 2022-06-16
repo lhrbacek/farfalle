091f1e967841c2678f1f10d2a7c3e2e10cd4af45
Veronika Hanulíková <xhanulik@fi.muni.cz>
2022-06-16 23:59:01 +0200

    feat: add swagger for ticket

--- backend/src/ticket/dto/filter-ticket.dto.ts
+++ backend/src/ticket/dto/filter-ticket.dto.ts
@@ -1,17 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDateString, IsInt, IsOptional, IsPositive } from 'class-validator';
 
 export class FilterTicketDto {
   @IsOptional()
   @IsDateString()
+  @ApiProperty()
   from?: Date;
 
   @IsOptional()
   @IsInt()
   @IsPositive()
+  @ApiProperty()
   user?: number;
 
   @IsOptional()
   @IsInt()
   @IsPositive()
+  @ApiProperty()
   performance?: number;
 }
--- backend/src/ticket/dto/update-ticket.dto.ts
+++ backend/src/ticket/dto/update-ticket.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { StatusTicket } from '@prisma/client';
 import { IsDateString, IsOptional, IsPositive } from 'class-validator';
 
 export class UpdateTicketDto {
   @IsOptional()
   @IsPositive()
+  @ApiProperty()
   price?: number;
 
   @IsOptional()
+  @ApiProperty()
   status?: StatusTicket;
 
   @IsOptional()
   @IsDateString()
+  @ApiProperty()
   reservedAt: Date;
 }
